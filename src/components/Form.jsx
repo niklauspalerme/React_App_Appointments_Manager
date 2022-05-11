fec85eb4f5304af743ddfc3edc77e96aa0d899fd
Palermo Rodriguez <nicolas.palermo@kcc.com>
2022-05-11 15:34:02 -0300

    51-52) States y leyendo datos para Form
    Se crea un respectiov state para leer los datos del Form como a su vez se capturan dichos datos y se van colocando al State

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,7 @@
 /// Importaciones
 
 
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////
@@ -10,24 +10,49 @@ import React, { Fragment } from 'react'
 
 
 const Form = () => {
+
+    /////////////////////////////////
+    //States
+
+    const [appointment, SetAppointment] = useState({
+        topic: '',
+        name:'',
+        date: '',
+        hour: '',
+        comments : ''
+    })
+
+    const {topic, name, date, hour, comments} = appointment;
+
+    /////////////////////////////////
+    //Funciones de Eventos
+
+    const actualizarState = e => {
+        SetAppointment({
+            ...appointment,
+            [e.target.name]: e.target.value
+        })
+    }
+
+
     return (  
        <Fragment>
            <h2>Create Appoinment</h2>
            <form action="">
                <label>Name of the Topic</label>
-               <input type="text" name='topic' placeholder='Write here...' className='u-full-width'/>
+               <input type="text" name='topic' placeholder='Write here...' className='u-full-width' onChange={actualizarState} value={topic}/>
 
                <label>Name of the Owner</label>
-               <input type="text" name='name' placeholder='Write here...' className='u-full-width'/>
+               <input type="text" name='name' placeholder='Write here...' className='u-full-width' onChange={actualizarState} value={name}/>
 
                <label>Date</label>
-               <input type="date" name='date' className='u-full-width'/>
+               <input type="date" name='date' className='u-full-width' onChange={actualizarState} value={date}/>
 
                <label>Hour</label>
-               <input type="time" name='hour' className='u-full-width'/>
+               <input type="time" name='hour' className='u-full-width' onChange={actualizarState} value={hour}/>
 
                <label>Comments</label>
-               <textarea name="comments" className='u-full-width'>
+               <textarea name="comments" className='u-full-width' onChange={actualizarState} value={comments}>
                </textarea>
 
                <button type='submit' className='u-full-width button-primary'>Add + </button>
